cd81ba66910abf4226e53ff42cfbcd65c8940c89
Shreyans Porwal <porwal@ucdavis.edu>
2025-11-11 01:25:09 -0800

    Setup DQNAgent for training run

--- test.py
+++ test.py
@@ -2,8 +2,7 @@
 
 import os
 from main import train, play
-from agents.random_agent import RandomAgent
-from config.random_config import RandomConfig
+from agents.deep_ql_agent import DQNAgent, DQNConfig
 from logging import getLogger
 
 # Disable the SDL audio driver warnings so there's no console warning output fo rit
@@ -12,8 +11,8 @@ logger = getLogger(__name__)
 
 def main():
     logger.info("In main.")
-    _ = train(RandomAgent, RandomConfig, False)
-    play(RandomAgent, RandomConfig, '', num_episodes=1)
+    _ = train(DQNAgent, DQNConfig, False)
+    #play(DQNAgent, DQNConfig, '', num_episodes=1)
 
 if __name__ == '__main__':
     main()
